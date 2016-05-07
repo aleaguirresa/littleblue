38744d150d6949f692ca63a01f3d092e820cd8f7
aleaguirresa <a.alejo@live.com.ar>
2016-05-06 23:25:48 -0300

    punto B
    Faltan detalles. Commiteo para avanzar

--- js/custom.js
+++ js/custom.js
@@ -1,9 +1,12 @@
 $("body").click(function(event) {
   if ($(event.target).attr('class') == 'open-menu' || $(event.target).parent().attr('class') == 'open-menu') {
-    $("body").toggleClass("collapse-body")
-  } else {
-  	if ($(event.target).parents('nav').attr('class') != 'side-menu' && $(event.target).attr('class') != 'side-menu') {
-	    $("body").removeClass("collapse-body")
-	}
+		$("body").addClass("collapse-body")
+		$(".menu-wrapper").addClass("hiddendino")
+		$('.fa-envelope').toggleClass('twist')
+  } else if ($(event.target).attr('class') == 'dino-open' || $(event.target).parent().attr('class') == 'dino-open') {
+		$(".menu-wrapper").toggleClass("hiddendino")
+		$('.fa-envelope').toggleClass('twist')
+  } else if ($(event.target).parents('nav').attr('class') != 'side-menu' && $(event.target).attr('class') != 'side-menu') {
+		$("body").removeClass("collapse-body")
   }
 });
